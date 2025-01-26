974cfbd6506dbe84ff0de3039695eb9401c6c0e1
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2025-01-26 00:46:45 -0800

    add error handling for fetching all dreams

--- src/app/api/dreams/route.ts
+++ src/app/api/dreams/route.ts
@@ -2,9 +2,15 @@ import { supabase } from "@/utils/supabase";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const fetchAllDreamsQuery = supabase.from("dreams").select();
-  const { data, error } = await fetchAllDreamsQuery;
-  if (error) throw error;
-  console.log(data);
-  return NextResponse.json(data, { status: 200 });
+  try {
+    const fetchAllDreamsQuery = supabase.from("dreams").select();
+    const { data, error } = await fetchAllDreamsQuery;
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
 }
