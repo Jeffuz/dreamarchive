6d54a243d5c9b1738c951112ba2def9aa7b7c1a6
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2025-01-26 14:33:12 -0800

    create endpoint for creating new account

---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,26 @@
+import { supabase } from "@/utils/supabase";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password, username } = await req.json();
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        data: {
+            username,
+        }
+      }
+    });
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+    return NextResponse.json(
+      { user: data.user, session: data.session },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
