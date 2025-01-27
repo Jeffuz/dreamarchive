d8d468de80305f371dae6167e5efeff7801ef1c4
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2025-01-26 16:40:39 -0800

    create endpoint for updating dream based on specific id

--- src/app/api/dreams/[id]/route.ts
+++ src/app/api/dreams/[id]/route.ts
@@ -1,15 +1,20 @@
 import { supabase } from "@/utils/supabase";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // Get specific dream
-export async function GET({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const id = (await params).id;
+
   if (!id) {
     return NextResponse.json(
       { error: "Dream ID is required" },
       { status: 400 }
     );
   }
+
   try {
     const fetchSpecificDream = supabase
       .from("dreams")
@@ -25,3 +30,44 @@ export async function GET({ params }: { params: Promise<{ id: string }> }) {
     return NextResponse.json({ error: error }, { status: 500 });
   }
 }
+
+// Update specific Dream
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const id = (await params).id;
+  if (!id) {
+    return NextResponse.json(
+      { error: "Dream ID is required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const { title, description } = await req.json();
+    if (!title && !description) {
+      return NextResponse.json({ error: "Nothing to update" }, { status: 400 });
+    }
+
+    const updateSpecificDream = supabase
+      .from("dreams")
+      .update({ title, description })
+      .eq("id", id)
+      .select()
+      .single();
+
+    const { data, error } = await updateSpecificDream;
+    if (error) {
+      return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+    return NextResponse.json(
+      { message: "Dream updated successfully", data },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
+
+// Delete specific dream
